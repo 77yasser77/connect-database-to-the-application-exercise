1dcacc52f255e616b64821b891cd2622f9323758
77yasser77 <yaseer.bawzier@gmail.com>
2025-06-26 16:11:04 +0300

    Add files via upload

---
+++ ContactsWinFormSolution/ContactsDataAccessLayer/clsDataAccessSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ContactsDataAccessLayer
+{
+    static class clsDataAccessSettings
+    {
+        public static string ConnectionString = "Server =.;DataBase=ContactsDB;Integrated Security = True; TrustServerCertificate=True";
+       
+
+    }
+}
